adb7a2a25464a757dbf2721e240aeab8d43dded2
waleswood <waleswood@126.com>
2014-12-11 11:00:52 +0800

    Update：抽中的不再被抽中。

--- Lottery/MainWindow.xaml.cs
+++ Lottery/MainWindow.xaml.cs
@@ -21,8 +21,17 @@ namespace Lottery
     public partial class MainWindow : Window
     {
         private DispatcherTimer timer = new DispatcherTimer();
+
         private int finalValue = 0;
 
+        private List<int> SelectedNumList = new List<int>();
+
+        private Random random = new Random();
+
+        /// <summary>
+        /// 抽奖人数
+        /// </summary>
+        public int Count = 15;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,7 +44,13 @@ namespace Lottery
         {
             if (numberGroupMain.IsStoped())
             {
-                textBoxFinalPrice.Text = "￥" + finalValue.ToString("F2");//显示最终金额
+                //textBoxFinalPrice.Text = "￥" + finalValue.ToString("F2");//显示最终金额
+                string str = "";
+                foreach (var item in this.SelectedNumList)
+                {
+                    str += item.ToString() + '，';
+                }
+                textBoxFinalPrice.Text = str;
                 timer.Stop();
                 buttonStart.IsEnabled = true;
             }
@@ -47,16 +62,36 @@ namespace Lottery
             buttonStart.IsEnabled = false;
             buttonStop.IsEnabled = true;
             numberGroupMain.TurnStart();
-            textBoxFinalPrice.Text = "";
+            //textBoxFinalPrice.Text = "";
             timer.Stop();
         }
 
         //停止按钮点击
         private void buttonStop_Click(object sender, RoutedEventArgs e)
         {
+            if (this.SelectedNumList.Count >= Count)
+            {
+                MessageBox.Show("还抽啥？直接发奖呗！");
+                e.Handled = true;
+                return;
+            }
+
             buttonStop.IsEnabled = false;
-            Random random = new Random();
-            finalValue = random.Next(15);
+            bool flag = true;
+            while (flag)
+            {
+                finalValue = random.Next(1,16);
+                if (this.SelectedNumList.Contains(finalValue) == false) //不在已抽中序列里面
+                {
+                    this.SelectedNumList.Add(finalValue);
+                    flag = false;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
             numberGroupMain.TurnStop(finalValue);//使数字组停止
             timer.Start();
         }
